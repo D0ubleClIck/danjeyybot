a6d8d92941dab8ad3b438ebfdc3827dfd6517a4d
Daniel Lambrecht <32965723+D0ubleClIck@users.noreply.github.com>
2018-01-30 14:27:12 +0100

    Update Statics.java

--- Version 2.0.1 (ALPHA)/src/main/java/util/Statics.java
+++ Version 2.0.1 (ALPHA)/src/main/java/util/Statics.java
@@ -7,10 +7,11 @@ import net.dv8tion.jda.core.entities.Guild;
  *@author Daniel Lambrecht
  *@version 1.0.0
  * */
+
 public class Statics {
-    public static Guild g;
+  
     public static final String version = "ALPHA 2.0.1";
-    public static final String TOKEN = "NDA2ODY5ODYwNjAyMDg1Mzg2.DU5OmA.EeC4mjUwQ9TDjRl_eRAbEYirTpQ";
+    public static final String TOKEN = "ENTER TOKEN HERE";
     public static final String PlayingGame = "created by Danjeyy | use -info | current version: ALPHA 2.0.1";
 
 
